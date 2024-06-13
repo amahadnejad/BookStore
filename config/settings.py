b9a42c93ca4471efcd05df7d04e1ae56133b9fd0
amahadnezhad <amahadnezhad@gmail.com>
2024-06-13 17:11:05 +0330

    Create CustomUser Model

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -131,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication Setting
+AUTH_USER_MODEL = 'accounts.CustomUser'
