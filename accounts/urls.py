91af63a73f6968da2fb768ad70b5d84950a4557e
amahadnezhad <amahadnezhad@gmail.com>
2024-06-14 17:58:08 +0330

    Create SignUpView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .views import SignUpView
+
 urlpatterns = [
-    # path('signup/', , name='signup'),
+    path('signup/',SignUpView.as_view() , name='signup'),
 ]
