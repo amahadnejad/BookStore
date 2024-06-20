706d745e02233338ebd871b792af6176789bba10
amahadnezhad <amahadnezhad@gmail.com>
2024-06-20 18:54:55 +0330

    Add BookListView

--- books/models.py
+++ books/models.py
@@ -10,4 +10,4 @@ class Book(models.Model):
     datetime_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.author}: {self.title}"
