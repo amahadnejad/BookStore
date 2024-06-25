8d14594e03ec5659c839f1cd2c11999004f4d8c7
amahadnezhad <amahadnezhad@gmail.com>
2024-06-25 11:55:34 +0330

    Update _base.html Template

--- config/settings.py
+++ config/settings.py
@@ -119,11 +119,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -140,3 +135,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Email Setting
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
