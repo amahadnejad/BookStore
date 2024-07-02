f28505d4da20a51bb6f8476d9117bff6cb872773
amahadnezhad <amahadnezhad@gmail.com>
2024-07-02 17:03:45 +0330

    Translate about_us.html Template

--- config/settings.py
+++ config/settings.py
@@ -143,6 +143,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+LOCALE_PATHS = (
+    'templates/locale',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
