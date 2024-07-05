ac321a15f72badc5739422eff19f7bc1417fe121
amahadnezhad <amahadnezhad@gmail.com>
2024-07-05 16:46:40 +0330

    Add 'add' Func

--- cart/cart.py
+++ cart/cart.py
@@ -8,10 +8,18 @@ class Cart:
         # Getting Cart From Session
         cart = self.session.get('cart')
 
-        # If User Don't have Cart , Create a New One
+        # If User Doesn't have Cart , Create a New One
         if not cart:
             cart = self.session['cart'] = {}
 
         self.cart = cart
+
+    def add(self, book, quantity=1):
+        book_id = str(book.id)
+
+        if book_id not in self.cart:
+            self.cart[book_id] = {'quantity': quantity}
+        else:
+            self.cart[book_id]['quantity'] += quantity
 
 
