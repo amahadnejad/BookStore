841924597de140aadecfaaba4799b6847faeffd5
amahadnezhad <amahadnezhad@gmail.com>
2024-07-05 17:50:45 +0330

    Add cart_detail.html Template

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls')),
     path('', include('pages.urls')),
+    path('cart/', include('cart.urls')),
     path('accounts/', include("allauth.urls")),
     path('rosetta/', include("rosetta.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
