ee5c74f6b590e5437c7a5e44b6f021753417748a
amahadnezhad <amahadnezhad@gmail.com>
2024-07-06 13:37:43 +0330

    Add cart_detail_view

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .cart import Cart
+from books.models import Book
+from .forms import AddToCartBookForm
 
 
 def cart_detail_view(request):
-    return render(request, 'cart/cart_detail.html')
+    cart = Cart(request)
+
+    return render(request, 'cart/cart_detail.html', {
+        'cart': cart,
+    })
+
+
