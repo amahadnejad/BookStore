ced8a58fba72d98c8108f265ed8ab4252b58053f
amahadnezhad <amahadnezhad@gmail.com>
2024-07-06 14:24:20 +0330

    Add remove_from_cart View

--- cart/cart.py
+++ cart/cart.py
@@ -39,7 +39,7 @@ class Cart:
         book_id = str(book.id)
 
         if book_id in self.cart:
-            del self.cart['book_id']
+            del self.cart[book_id]
             self.save()
 
     def __iter__(self):
--- cart/urls.py
+++ cart/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
-from .views import cart_detail_view, add_to_cart_view
+from .views import cart_detail_view, add_to_cart_view, remove_from_cart
 
 app_name = 'cart'
 
 urlpatterns = [
     path('', cart_detail_view, name="cart_detail"),
     path('add/<int:book_id>/', add_to_cart_view, name="cart_add"),
+    path('remove/<int:book_id>/', remove_from_cart, name="cart_remove"),
 
 ]
