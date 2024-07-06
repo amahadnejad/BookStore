c3d3910d6f17db08b288490133026f75722d01a5
amahadnezhad <amahadnezhad@gmail.com>
2024-07-06 13:38:16 +0330

    Add add_to_cart_view

--- cart/views.py
+++ cart/views.py
@@ -13,3 +13,15 @@ def cart_detail_view(request):
     })
 
 
+def add_to_cart_view(request, book_id):
+    cart = Cart(request)
+    book = get_object_or_404(Book, id=book_id)
+    form = AddToCartBookForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quantity = cleaned_data['quantity']
+        cart.add(book, quantity)
+
+    return redirect('cart:cart_detail')
+
