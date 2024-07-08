f7b4c9147a8a9e8b03dd44e9f7b28873785f9efe
amahadnezhad <amahadnezhad@gmail.com>
2024-07-08 14:49:53 +0330

    Configure orders/admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,25 @@
+
 from django.contrib import admin
+from jalali_date.admin import ModelAdminJalaliMixin
 
 from .models import Order, OrderItem
 
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    fields = ['order', 'book', 'quantity', 'price', ]
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'datetime_created', 'is_paid', ]
+
+    inlines = [
+        OrderItemInline,
+    ]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'book', 'quantity', 'price', ]
