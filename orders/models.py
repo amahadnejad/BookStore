ca15897c406ab42d1fd6b88acdf11517e0a25f31
amahadnezhad <amahadnezhad@gmail.com>
2024-07-08 14:36:48 +0330

    Add Order Model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    phone_number = models.CharField(max_length=11)
+    country = models.CharField(100)
+    city = models.CharField(100)
+    address = models.CharField(max_length=700)
+
+    is_paid = models.BooleanField(default=False)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
