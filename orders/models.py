ba570f6309612c9866cec6b86365a0ed88f1cd6d
amahadnezhad <amahadnezhad@gmail.com>
2024-07-08 14:47:01 +0330

    Add OrderItem Model

--- orders/models.py
+++ orders/models.py
@@ -13,7 +13,21 @@ class Order(models.Model):
     city = models.CharField(100)
     address = models.CharField(max_length=700)
 
+    order_notes = models.TextField()
     is_paid = models.BooleanField(default=False)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Order: {self.id}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    book = models.ForeignKey('books.Book', on_delete=models.PROTECT, related_name='order_items')
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'OrderItem: {self.id} Of Order: {self.order.id}'
