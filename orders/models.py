4e1fa51796981e682fb1af29f0a62c5ad0f8bbc7
amahadnezhad <amahadnezhad@gmail.com>
2024-07-15 01:03:08 +0330

    Translate Order Model

--- orders/models.py
+++ orders/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100, verbose_name='FirstName')
-    last_name = models.CharField(max_length=100, verbose_name='LastName')
-    email = models.EmailField(verbose_name='Email')
+    first_name = models.CharField(max_length=100, verbose_name=_('FirstName'))
+    last_name = models.CharField(max_length=100, verbose_name=_('LastName'))
+    email = models.EmailField(verbose_name=_('Email'))
 
-    phone_number = models.CharField(max_length=11, verbose_name='PhoneNumber')
-    country = models.CharField(max_length=100, verbose_name='Country')
-    city = models.CharField(max_length=100, verbose_name='City')
-    address = models.CharField(max_length=700, verbose_name='Address')
+    phone_number = models.CharField(max_length=11, verbose_name=_('PhoneNumber'))
+    country = models.CharField(max_length=100, verbose_name=_('Country'))
+    city = models.CharField(max_length=100, verbose_name=_('City'))
+    address = models.CharField(max_length=700, verbose_name=_('Address'))
 
-    order_notes = models.CharField(max_length=700, verbose_name='Notes', blank=True)
+    order_notes = models.CharField(max_length=700, verbose_name=_('Notes'), blank=True)
     is_paid = models.BooleanField(default=False)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
