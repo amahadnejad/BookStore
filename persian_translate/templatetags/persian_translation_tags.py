3462c10bb7f6fe49b555bd8a3292f22b7b4d07b4
amahadnezhad <amahadnezhad@gmail.com>
2024-07-23 20:17:10 +0330

    Use translate_number Filter In book_list.html Template

--- persian_translate/templatetags/persian_translation_tags.py
+++ persian_translate/templatetags/persian_translation_tags.py
@@ -3,6 +3,7 @@ from django import template
 register = template.Library()
 
 
+@register.filter
 def translate_number(value):
     value = str(value)
     english_to_persian = value.maketrans('0123456789', '۰۱۲۳۴۵۶۷۸۹')
