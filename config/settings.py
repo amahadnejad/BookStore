d4ca5d12289cc41da517d0328c602b40bce9ad63
amahadnezhad <amahadnezhad@gmail.com>
2024-10-01 16:18:03 +0330

    Update Environment Variables

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DJANGO_DEBUG_KEY')
 
 ALLOWED_HOSTS = []
 
@@ -137,7 +137,6 @@ LANGUAGES = (
     ('fa', 'Persian'),
 )
 
-# TIME_ZONE = 'UTC'
 TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
